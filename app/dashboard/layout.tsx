71d3877b1ae6fea60d5e9887cd2b66feb0946194
Isha4002 <ishapal2004@gmail.com>
2026-05-02 01:47:25 +0530

    05-Dashboard-UI

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,48 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { getAllPlaygroundForUser } from "@/modules/dashboard/actions";
+import { Playground, Templates } from "@prisma/client";
+import { DashboardSidebar } from "@/modules/dashboard/actions/components/dashboard-sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+type TemplateType = "REACT" | "NEXTJS" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR";
+
+export default async function DashboardLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+
+  const playgroundData = await getAllPlaygroundForUser();
+
+  const technologyIconMap: Record<TemplateType, string> = {
+    REACT: "Zap",
+    NEXTJS: "Lightbulb",
+    EXPRESS: "Database",
+    VUE: "Compass",
+    HONO: "FlameIcon",
+    ANGULAR: "Terminal",
+  };
+
+  const formattedPlaygroundData = (playgroundData as Playground[])?.map((item) => ({
+    id: item.id,
+    name: item.title,
+    starred: false,
+    icon: technologyIconMap[item.template as TemplateType] || "Code2"
+  })) || [];
+
+  return (
+    <TooltipProvider>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full overflow-x-hidden">
+
+        <DashboardSidebar initialPlaygroundData={formattedPlaygroundData} />
+
+        <main className="flex-1">
+          {children}
+        </main>
+
+      </div>
+    </SidebarProvider>
+    </TooltipProvider>
+  );
+}
