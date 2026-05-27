02a6e66e99b12224c5a00a6c821dd4a5ee3f0aa1
Isha4002 <ishapal2004@gmail.com>
2026-05-27 14:28:16 +0530

    09-monaco-editor-part-1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,27 +2,53 @@
 
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
 import { TemplateFileTree } from '@/modules/playground/components/playground-explorer';
+import { useFileExplorer } from '@/modules/playground/hooks/useFileExplorer';
 import { usePlayground } from '@/modules/playground/hooks/usePlayground';
+import { TemplateFile } from '@/modules/playground/lib/path-to-json';
 import { Separator } from '@/vibecode-starters/vite-shadcn/src/components/ui/separator';
 import { TooltipProvider } from '@/vibecode-starters/vite-shadcn/src/components/ui/tooltip';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MainPlaygroundPage = () => {
     const {id} = useParams<{id: string}>();
 
     const {playgroundData, templateData, isLoading, error, loadPlayground} = usePlayground(id);
 
-    console.log("Playground Data:", playgroundData);
-    console.log("Template Data:", templateData);
+    const{
+        selectedFileId,
+        closeAllFiles,
+        openFile,
+        openFiles,
+        setTemplateData,
+        setPlaygroundId,
+        setSelectedFileId,
+        setOpenFiles,
 
-    const activeFile = "sample.txt"; 
+
+    } = useFileExplorer();
+
+    useEffect(() => {setPlaygroundId(id)}, [id, setPlaygroundId]);
+
+    useEffect(() => {
+        if(templateData && !openFiles.length){
+            setTemplateData(templateData);
+        }
+    },[templateData, setTemplateData, openFiles.length])
+
+
+    const activeFile = openFiles.find((file) => file.id === selectedFileId) ; 
+    const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
+
+    const handleFileSelect = (file:TemplateFile) => {
+        openFile(file);
+    }
     return (
         <TooltipProvider>
             <>
             <TemplateFileTree
             data={templateData!}
-               onFileSelect={()=>{}}
+               onFileSelect={handleFileSelect}
                selectedFile={activeFile}
                title="File Explorer"
                onAddFile={()=>{}}//wrappedHandleAddFile} 
