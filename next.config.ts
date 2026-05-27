3942a1fb6056182dac62d22c8d4976a3f91aa3b0
Isha4002 <ishapal2004@gmail.com>
2026-05-27 23:47:54 +0530

    10-webcontainers-part-1

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,36 @@ const nextConfig: NextConfig = {
   images:{
     remotePatterns:[
       {
-        protocol:"https",
-        hostname: "lh3.googleusercontent.com",
+        // protocol:"https",
+        // hostname: "lh3.googleusercontent.com",
         
-        pathname:"/**"
+        // pathname:"/**"
+        protocol: "https",
+        hostname: "*",
+        port:'',
+        pathname: "/**"
       }
     ]
-  }
+  },
+  async headers(){
+    return [
+      {
+        source:'/:path*',
+        headers:[
+          {
+          key:'Cross-Origin-Opener-Policy',
+          value:'same-origin',
+          },
+          {
+            key:'Cross-Origin-Embedder-Policy',
+            value:'require-corp',
+          },
+
+        ],
+      },
+    ]
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
